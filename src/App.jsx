0500993cf9e2804452fba944a9c3b2eada66ab13
agasurfer <agasurfer@hotmail.com>
2024-12-12 15:00:32 +0100

    chge router

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import HomePage from './pages/HomePage';
 import AddProduct from './pages/AddProduct';
 import EditProduct from './pages/EditProduct';
 
+
 const App = () => {
   return (
-    <Router>
+      <>
       <Routes>
         <Route path="/" element={ <HomePage /> } />
         <Route path="/add-product" element={ <AddProduct /> } />
          <Route path="/edit-product/:id" element={ <EditProduct /> } />
       </Routes>
       <ToastContainer />
-    </Router>
+      </>
   )
 }
 
